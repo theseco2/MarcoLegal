54ecba5b5d9d4ac98cb73b56bc0923ceb0474863
Marvin Estuardo Chacón Orellana <marvin.chacon-91@outlook.com>
2021-08-22 11:03:02 -0600

    Update ctltitulo.php

--- controladores/ctltitulo.php
+++ controladores/ctltitulo.php
@@ -9,13 +9,13 @@ class Titulo {
 	/*=============================================
 	 METODO GUARDAR O EDITAR
 	=============================================*/
-	static public function ctlguardareditar($idtitulo,$idregion,$nombre){
+	static public function ctlguardareditar($idtitulo,$idley,$nombre){
 
 		if (empty($idtitulo)){
-			return Titulo::insertar($idregion,$nombre);
+			return Titulo::insertar($idley,$nombre);
 		}
 		else {
-			return Titulo::editar($idtitulo,$idregion,$nombre);
+			return Titulo::editar($idtitulo,$idley,$nombre);
 		}
 
 	}
@@ -23,22 +23,22 @@ class Titulo {
 	/*=============================================
 	 METODO ELIMINAR
 	=============================================*/
-	static public function eliminar_controlador($idtitulo,$idregion){
-		return Titulo::eliminar($idtitulo,$idregion);
+	static public function eliminar_controlador($idtitulo,$idley){
+		return Titulo::eliminar($idtitulo,$idley);
 	}
 	
 	/*=============================================
 	 METODO MOSTRAR
 	=============================================*/
-	static public function mostrar_controlador($idtitulo,$idregion){
-		return Titulo::mostrar($idtitulo,$idregion);
+	static public function mostrar_controlador($idtitulo,$idley){
+		return Titulo::mostrar($idtitulo,$idley);
 	}	
 	
 	/*=============================================
 	 METODO LISTAR
 	=============================================*/
-	static public function listar_controlador($idregion){
-		$rspta = Titulo::listar($idregion);
+	static public function listar_controlador($idley){
+		$rspta = Titulo::listar($idley);
  		//Vamos a declarar un array
  		$data= Array();
 		
@@ -49,7 +49,7 @@ class Titulo {
  			$data[]=array(
  				"0"=>'<button class="btn btn-warning" onclick="mostrar('.$reg->ID.')"><i class="fa fa-pencil"></i></button>'.
  					' <button class="btn btn-danger" onclick="verificamuni('.$reg->ID.')"><i class="fa fa-trash"></i></button>'.
-					'<a target="_self" href="'.$url.$reg->ID.$url2.$idregion.'"> <button class="btn btn-success">Municipios</button></a>',
+					'<a target="_self" href="'.$url.$reg->ID.$url2.$idley.'"> <button class="btn btn-success">Municipios</button></a>',
 				"1"=>$reg->NOMREGION,
  				"2"=>$reg->NOMBRE
  				);
@@ -66,8 +66,8 @@ class Titulo {
 	/*=============================================
 	 METODO MOSTRAR
 	=============================================*/
-	static public function verificamuni_controlador($idtitulo,$idregion){
-		return Titulo::verificamuni($idtitulo,$idregion);
+	static public function verificamuni_controlador($idtitulo,$idley){
+		return Titulo::verificamuni($idtitulo,$idley);
 	}	
 }
 
